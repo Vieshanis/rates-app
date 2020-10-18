dc2a50713b053538e30d0f0c2082ef33bc519ed2
viesturs <viesturs.berzins@hotmail.com>
2020-10-18 11:50:39 +0300

    Rates period test

--- src/app/rates-period/rates-period.component.spec.ts
+++ src/app/rates-period/rates-period.component.spec.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { async, ComponentFixture, fakeAsync, TestBed, tick, waitForAsync } from '@angular/core/testing';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
@@ -11,7 +12,8 @@ import { MatTableModule } from '@angular/material/table';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import * as moment from 'moment';
 import { of } from 'rxjs';
-import { GetRatesPeriodData } from '../shared/data';
+import { debounceTime, delay, take } from 'rxjs/operators';
+import { FormatedRatesData, GetRatesPeriodData } from '../shared/data';
 import { GLOBALS } from '../shared/globals';
 import { RatesPeriodRoutingModule } from './rates-period-routing.module';
 
@@ -63,6 +65,7 @@ describe('RatesPeriodComponent', () => {
       startDate: new FormControl(date),
       endDate: new FormControl(date),
     });
+
     component.form.controls.startDate.setValue(date);
     component.form.controls.endDate.setValue(date);
     fixture.detectChanges();
@@ -70,42 +73,16 @@ describe('RatesPeriodComponent', () => {
     expect(component.form.value).toEqual(formGroup.value);
   });
 
-  it('should call ratesPeriodService.getRatesPeriod after ngOnInit', fakeAsync(() => {
-    tick(300);
-    expect(getRatesPeriodSpy).toHaveBeenCalled();
+  it('should call ratesPeriodService.getRatesPeriod after ngOnInit', waitForAsync(() => {
+    fixture.whenStable().then(() => {
+      expect(getRatesPeriodSpy).toHaveBeenCalledTimes(1);
+    });
   }));
 
-  // it('should call ratesPeriodService.getRatesPeriod after ngOnInit with correct values', () => {
-  //   expect(getRatesPeriodSpy).toHaveBeenCalledWith('EUR', moment().format(GLOBALS.dateFormat));
-  // });
-
-  // it('should call ratesPeriodService.getRatesPeriod on form.controls.base.valueChange', () => {
-  //   component.form.controls.base.setValue('AUD');
-  //   // Because of async pipe ratesPeriodService.getRatesPeriodPeriodSpy will be already called once after ngOnInit
-  //   expect(getRatesPeriodSpy).toHaveBeenCalledTimes(2);
-  // });
-
-  // it('should call ratesPeriodService.getRatesPeriod on form.controls.date.valueChange with correct value', () => {
-  //   component.form.controls.date.setValue('2020-10-15T15:11:22.341Z');
-  //   expect(getRatesPeriodSpy).toHaveBeenCalledTimes(2);
-  // });
-
-  // it('should not call ratesPeriodService.getRatesPeriod on form.controls.date.valueChange with value past min date', () => {
-  //   component.form.controls.date.setValue('1850-10-15T15:11:22.341Z');
-  //   expect(getRatesPeriodSpy).toHaveBeenCalledTimes(1);
-  // });
-
-  // it('should not call ratesPeriodService.getRatesPeriod on form.controls.date.valueChange with value past max date', () => {
-  //   component.form.controls.date.setValue('2220-10-15T15:11:22.341Z');
-  //   expect(getRatesPeriodSpy).toHaveBeenCalledTimes(1);
-  // });
-
-  // it('should return filtered and sorted data after calling ratesPeriodService.getRatesPeriod', done => {
-  //   component.rates$.subscribe(res => {
-  //     expect(res).toEqual(FormatedRatesData);
-  //     done();
-  //   });
-  // });
-
+  it('should call ratesPeriodService.getRatesPeriod after ngOnInit with correct values', waitForAsync(() => {
+    fixture.whenStable().then(() => {
+      expect(getRatesPeriodSpy).toHaveBeenCalledWith('EUR', moment().format(GLOBALS.dateFormat), moment().format(GLOBALS.dateFormat));
+    });
+  }));
 
 });
